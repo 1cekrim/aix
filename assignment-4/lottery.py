b5486cf25a0fe05c795fb8932bba1e99504424a4
1cekrim <1cekrim.dev@gmail.com>
2019-11-13 09:26:25 +0900

    feat: Implement add_fake_date

--- assignment-4/lottery.py
+++ assignment-4/lottery.py
@@ -3,6 +3,7 @@ import random as rnd
 import sys
 from functools import reduce
 
+
 class Lottery:
     def __init__(self, file_name: str):
         self.dataframe = pd.read_csv(file_name)
@@ -17,25 +18,37 @@ class Lottery:
     def add_column(self, column_name: str, data: list):
         self.dataframe[column_name] = data
 
+    def add_fake_data(self):
+        s = [sorted(i) for i in map(lambda j: j[2:9],
+                                    self.dataframe.values.tolist())]
+        result = []
+        for data in self.dataframe.values.tolist():
+            while True:
+                numbers = rnd.sample([i for i in range(1, 46)], 7)
+                sorted_numbers = sorted(numbers)
+                if not reduce(lambda a, b: a or b, [k == sorted_numbers for k in s], False):
+                    result.append(data[0:2] + numbers + [0])
+                    break
+        self.dataframe = self.dataframe.append(pd.DataFrame(
+            result, columns=self.dataframe.columns)).sort_values(by=['round', 'win'], ascending=False)
+
+    def print_dataframe(self):
+        print(self.dataframe[0:20])
+
     def make_csv(self, name: str):
         self.dataframe.to_csv(name, mode='w')
 
+
 def main():
     lottery = Lottery(sys.argv[1])
-    lst = lottery.count_number(
-        ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'bonus'])
-    
-    for number, count in sorted(lst, key=lambda t: t[1], reverse=True):
-        print(f'{number} -> {count} times')
 
-    lottery.add_column('win', [rnd.choice([0, 1])
+    lottery.add_column('win', [1
                                for _ in range(len(lottery.dataframe))])
-    lottery.add_column('weather', [rnd.choice(
-        ['clear', 'cloudy', 'warm', 'cool']) for _ in range(len(lottery.dataframe))])
 
-    lottery.make_csv(sys.argv[1].split('.')[0] + '-result.csv')
+    lottery.add_fake_data()
+
+    lottery.print_dataframe()
 
 
 if __name__ == "__main__":
     main()
-
