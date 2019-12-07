d5c00f693e857415b9f6aa47576b9a8382b0519e
1cekrim <1cekrim.dev@gmail.com>
2019-12-07 20:26:14 +0900

    Update

--- assignment-5/obtain_data.py
+++ assignment-5/obtain_data.py
@@ -1,4 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from ast import literal_eval
 
 df = pd.read_csv('crime.csv', engine='python')
-print(df['OFFENSE_CODE_GROUP'].value_counts().iloc[:10])
+
+df = df[df['Location'] != '(0.00000000, 0.00000000)']
+df = df[df['Location'] != '(-1.00000000, -1.00000000)']
+
+
+
+
+# transposed = np.array([literal_eval(i) for i in df['Location']]).T
+
+# for i in transposed[0]:
+#     if i < 10:
+#         print(i)
+
+# plt.scatter(transposed[0], transposed[1])
+# plt.show()
