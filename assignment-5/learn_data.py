9fd686afb326830cde517e5c35c1881e35f5b35b
1cekrim <1cekrim.dev@gmail.com>
2019-12-09 01:08:14 +0900

    update

--- assignment-5/learn_data.py
+++ assignment-5/learn_data.py
@@ -9,14 +9,25 @@ target_column = ['DISTRICT', 'YEAR', 'MONTH', 'DAY_OF_WEEK', 'HOUR', 'CLASSIFICA
 
 df = pd.read_csv('fixed_crime6.csv', engine='python')
 
-one_hot_index = {}
+# one_hot_index = {}
 
-cnt = 0
+# cnt = 0
 
-for i in target_column:
-    one_hot_index[i] = {}
-    for c in df[i].unique():
-        one_hot_index[i][c] = cnt
-        cnt += 1
+# for i in target_column:
+#     one_hot_index[i] = {}
+#     for c in df[i].unique():
+#         one_hot_index[i][c] = cnt
+#         cnt += 1
 
-print(one_hot_index)
+# one_hot = []
+
+# for s in df.iterrows():
+#     for c in target_column:
+#         result = [0 for _ in range(cnt)]
+#         result[one_hot_index[c][s[1][c]]] = 1
+#         one_hot.append(result)
+
+
+
+
+print('success')
